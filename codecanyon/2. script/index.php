ae16eaa08a77cbf693507d705a96fc4afa7d5f7c
Ali <alinawaz254@gmail.com>
2026-03-19 15:39:07 +0500

    removed extra folders.

--- codecanyon/2. script/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-	//This page is only redirection to login.php you can do anything on this page as per your needs.
-	HEADER( 'LOCATION: login.php' );
--- codecanyon/2. script/lib/includes/update.php
+++
@@ -1,6 +0,0 @@
-<?php
-	if(get_option('version') < '4.0') { 
-		set_option('version', '4.0');
-		
-		//This will work when Version is less than 4
-	}
