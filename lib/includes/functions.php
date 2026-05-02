ba322378a7e1ab099b0767495284bc5339e644a6
Ali <alinawaz254@gmail.com>
2026-05-02 16:33:00 +0500

    minor changes

--- lib/includes/functions.php
+++ lib/includes/functions.php
@@ -186,13 +186,11 @@
 			send_mail_smtp( $mailto, $subject, $message );
 		} else {
 			//getting set email addresses from database.
-			// $from_email = get_option('email_from');
-			$from_email = 'BizProMax';
-			$reply_to = 'BizProMax';
-			// $reply_to = get_option('email_to');
+			$from_email = get_option('email_from');
+			$reply_to = get_option('email_to');
 	
-            $mailheaders = "From:".$from_email;
-            $mailheaders .="Reply-To:".$reply_to;
+            $mailheaders = "From BizProMax :".$from_email;
+            $mailheaders .="Reply-To BizProMax :".$reply_to;
 			$from = $from_email;
 			$filename = '';
 			$fileatt_type = '';
