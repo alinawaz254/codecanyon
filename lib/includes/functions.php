32972ce1185b3f9f4ce2c29cce7520b6d2eb0ed3
Ali <alinawaz254@gmail.com>
2026-05-05 15:35:03 +0500

    minor mail changes

--- lib/includes/functions.php
+++ lib/includes/functions.php
@@ -247,7 +247,7 @@
 		if ( empty( get_option('email_from') ) ) {
 			$error = _( 'Missing email from' );
 		}
-		$mail->setFrom(get_option('email_from'), '');
+		$mail->setFrom("BizProMax <". get_option('email_from') . ">", '');
 
 		if ( ! empty( get_option('email_to') ) ) {
 			$mail->addReplyTo(get_option('email_to'), '');
